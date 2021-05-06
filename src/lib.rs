798d37d2fef89bff662b3ebc3a281580428ee7d2
John Wells <john@attackgoat.com>
2021-05-06 19:17:26 -0400

    Fix for pointer handling issue

--- src/lib.rs
+++ src/lib.rs
@@ -4,23 +4,9 @@
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
-
-pub fn str_as_nk_string<S>(s: S) -> NkString
-where
-    S: AsRef<str>,
-{
-    let s_ref = s.as_ref();
-
+pub fn str_as_nk_string(s: &str) -> NkString {
     NkString {
-        p: s_ref.as_ptr() as *const _,
-        n: s_ref.len() as _,
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+        p: s.as_ptr() as *const _,
+        n: s.len() as _,
     }
 }
